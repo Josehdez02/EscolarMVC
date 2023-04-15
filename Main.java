64baf4073f7f0755e853b9cc83c29f0b208a1bc5
gissell <l21020451@veracruz.tecnm.mx>
2023-04-15 14:28:19 -0600

    Main Correcto

--- Main.java
+++ Main.java
@@ -5,13 +5,7 @@ import modelo.ModeloAlumno;
 import vista.*;
 public class Main {
     public static void main(String[] args) {
-        //ImprimirCatedratico ic= new ImprimirCatedratico();
         VentanaPrincipal v1=new VentanaPrincipal();
         ControladorVentanaPrincipal c1=new ControladorVentanaPrincipal(v1);
-       // ImprimirCatedratico v2 =new ImprimirCatedratico();
-        //ControladorCatedratico c2 =new ControladorCatedratico(v2);
-        //VentanaAlumno v2=new VentanaAlumno();
-        //ModeloAlumno m1=new ModeloAlumno();
-        //ControladorAlumno c2=new ControladorAlumno(m1,v2);
     }
 }
